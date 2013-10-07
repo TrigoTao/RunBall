46f914de450b9e5192eb2e1a48b8b142026f5fad
txcodelife <tx52@hotmail.com>
2013-10-07 21:25:12 +0800

    change Line

--- Line.js
+++ Line.js
@@ -7,7 +7,6 @@ Crafty.c('Line',{
      },
  
      line: function(point_b, point_e, lineWidth, color) {
-        this.w = this.h = radius * 2 + 2;
         this.color = color || "#000000";
         this.point_e = point_e;
         this.point_b = point_b;
@@ -25,13 +24,8 @@ Crafty.c('Line',{
         ctx.strokeStyle = this.color;
         ctx.lineWidth = this.lineWidth;
         ctx.beginPath();
-        ctx.arc(
-            this.x + this.radius + 1,
-            this.y + this.radius + 1,
-            this.radius,
-            this.pi_b,
-            this.pi
-        );
+        ctx.moveTo(this.point_b[0],this,point_b[1]);
+        ctx.lineTo(this.point_e[0],this.point_e[1]);
         ctx.stroke();
         ctx.closePath();
         //ctx.fill();
@@ -39,31 +33,19 @@ Crafty.c('Line',{
     },
 
     // they > 0
-    arcPointData: function ArcPointData(radius, pi_b, pi, width, accuracy){
+    linePointData: function(point_b, point_e, width, accuracy){
         var p = (accuracy || 61) - 1, d = [], width = width || 5;
         //console.log(pi_b,Math.cos(pi_b));
         //console.log(pi,Math.cos(pi));
-        offset = radius + 1;
-        radius -= width / 2;
-        for(var i=0, a=pi_b; i<=p; i++){ 
-            a = (i/p)*(pi - pi_b)+pi_b;
-            d[i] = [Math.cos(a)*radius + offset , Math.sin(a)*radius + offset]; 
-        }
-        radius += width;
-        for(var i=p, a=pi; i<=p+p; i++){
-            a = pi - (i/p - 1)*(pi - pi_b)
-            d[i+1] = [Math.cos(a)*radius + offset, Math.sin(a)*radius + offset];
-        }
         return d; 
     },
 
-    // if the point is a ball(2D), r is its radius
     containsPoint: function(x,y,r){
         dia = (r || 0) * 2;
         if(this.lineWidth > dia){
-            var allPoints = new Crafty.polygon(this.arcPointData(this.radius, this.pi_b, this.pi, this.lineWidth - dia));
+            var allPoints = new Crafty.polygon(this.linePointData(this.point_b, this.point_e, this.lineWidth - dia));
 
-            allPoints.shift(this.x,this.y);
+            //allPoints.shift(this.x,this.y);
             return allPoints.containsPoint(x,y);
         }
         else{
