91ad9db5c90975934d888aea6b55b9a6d693940c
txcodelife <tx52@hotmail.com>
2013-10-16 20:19:41 +0800

    rm unused block

--- src/Experiment.js
+++ src/Experiment.js
@@ -9,14 +9,7 @@ Experiment = {
         Crafty.audio.add("warn", "workspace/2HAND/assets/sounds/beep-1.mp3");
        
         //Paddles
-        var track = Crafty.e("Track")
-            .push( Crafty.e("Arc")
-                    .attr({ x: 50, y: 150 })
-                    .arc(50, Math.PI/2, Math.PI*3/2, 20, 'green')
-            )
-            .push( Crafty.e("Line").line([300,150],[100,150],20,'green'))
-            //.push( Crafty.e("Line").line([200,20],[40,40],1,'black'))
-            //.push( Crafty.e("Line").line([198.7,10.0],[38.7,30.0],1,'black') );
+        var track = Crafty.e("Track");
 
         for (i = 0; i < config_jigsaw.length; i++) {
             item = config_jigsaw[i];
@@ -36,15 +29,6 @@ Experiment = {
                     break;
             }
         }
-
-        Crafty.e("Paddle, 2D, Canvas, Color, Collision,WiredHitBox")
-            .color('rgb(0,255,0)')
-            .attr({ x: 580, y: 100, w: 10, h: 100 })
-            .collision()
-            .onHit('Ball',function(){
-                console.log('right');
-            });
-        
 
         Crafty.e("Ball, Fourway")
             .appear(250,50,5)
